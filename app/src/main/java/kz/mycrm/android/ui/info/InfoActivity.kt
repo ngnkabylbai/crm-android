388750660e915bd73b0d43c8877d4810251b3fcd
Unknown <w.aset.kz@gmail.com>
2017-12-08 13:19:22 +0600

    wip

--- app/src/main/java/kz/mycrm/android/ui/info/InfoActivity.kt
+++ app/src/main/java/kz/mycrm/android/ui/info/InfoActivity.kt
@@ -2,6 +2,8 @@ package kz.mycrm.android.ui.info
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -9,6 +11,7 @@ import butterknife.BindView
 import butterknife.ButterKnife
 import butterknife.OnClick
 import kz.mycrm.android.R
+import kz.mycrm.android.ui.division.DivisionAdapter
 
 class InfoActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -17,11 +20,22 @@ class InfoActivity : AppCompatActivity(), View.OnClickListener {
     @BindView(R.id.info_time)
     lateinit var time:TextView
 
+    lateinit var rv:RecyclerView
+    lateinit var lm:LinearLayoutManager
+    lateinit var adapter:ServiceAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
 
         ButterKnife.bind(this)
+
+        rv = findViewById(R.id.service_list)
+        rv.setHasFixedSize(true)
+        lm = LinearLayoutManager(this)
+        adapter = ServiceAdapter(serviceList, this)
+        rv.setLayoutManager(lm)
+        rv.setAdapter(adapter)
     }
 
     @OnClick(R.id.close_activity)
