ed959597743ba8846e96fb0bcc0af7065c887fbd
NKabylbay <nkabylbai@gmail.com>
2017-12-26 12:12:18 +0600

    Fix calendar goToday()

--- horizontalcalendar/src/main/java/devs/mulham/horizontalcalendar/DateComparator.java
+++ horizontalcalendar/src/main/java/devs/mulham/horizontalcalendar/DateComparator.java
@@ -1,20 +1,26 @@
 package devs.mulham.horizontalcalendar;
 
-import android.text.format.DateUtils;
-
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Comparator;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by Nurbek Kabylbay on 22.11.2017.
  */
 
 public class DateComparator implements Comparator<Date> {
-    protected static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
 
     public int compare(Date d1, Date d2) {
-        return DATE_FORMAT.format(d1).compareTo(DATE_FORMAT.format(d2));
+        try {
+            return DATE_FORMAT.parse(DATE_FORMAT.format(d1)).compareTo(DATE_FORMAT.parse(DATE_FORMAT.format(d2)));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return 1;
     }
 }
