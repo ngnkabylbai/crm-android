dab678a408a12c4b1ef3ae337ce536f8098a3b2a
nG <nkabylbai@gmail.com>
2018-02-03 10:50:30 +0600

    Add intro check into shared preferences

--- app/src/main/java/kz/mycrm/android/SplashActivity.kt
+++ app/src/main/java/kz/mycrm/android/SplashActivity.kt
@@ -4,28 +4,26 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.ConnectivityManager
 import android.os.Bundle
-import android.os.Handler
 import android.widget.Toast
 import kz.mycrm.android.ui.BaseActivity
 import kz.mycrm.android.ui.intro.IntroActivity
 import kz.mycrm.android.ui.login.loginIntent
 import kz.mycrm.android.ui.main.division.divisionsIntent
-import kz.mycrm.android.util.ApiUtils
-
-fun Context.splashIntent(): Intent {
-    return Intent(this, SplashActivity::class.java)
-}
 
 class SplashActivity : BaseActivity() {
 
     private lateinit var viewModel: SplashViewModel
     private var startMain = false
+    private lateinit var sharedPref: SharedPreferences
 
     //    TODO: Refactor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sharedPref = getSharedPreferences(getString(R.string.app_mycrm_shared_key), Context.MODE_PRIVATE)
 
         viewModel = ViewModelProviders.of(this).get(SplashViewModel::class.java)
         viewModel.checkAuthentication().observe(this, Observer { isAuthenticated ->
@@ -34,21 +32,43 @@ class SplashActivity : BaseActivity() {
             }
         })
 
-        startActivityForResult(Intent(this, IntroActivity::class.java), 1)
+        if(BuildConfig.DEBUG || wasIntroShown()) {
+            startActivityForResult(Intent(this, IntroActivity::class.java), 1)
+        } else {
+            loadNextActivity()
+        }
+    }
+
+    private fun loadNextActivity() {
+        if (startMain) {
+            if (isInternetAvailable()) {
+                startActivity(divisionsIntent())
+                finish()
+            } else {
+                Toast.makeText(this, "Нет подключения к сети", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            startActivity(loginIntent())
+            finish()
+        }
+    }
+
+    private fun wasIntroShown(): Boolean {
+        val defaultValue = getString(R.string.app_mycrm_shared_intro_default)
+        val sharedValue = sharedPref.getString(getString(R.string.app_mycrm_shared_intro), defaultValue)
+
+        return sharedValue == defaultValue
+    }
+
+    private fun saveState() {
+        val editor = sharedPref.edit()
+        editor.putString(getString(R.string.app_mycrm_shared_intro), getString(R.string.app_mycrm_shared_intro_shown))
+        editor.apply()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-            if (startMain) {
-                if (isInternetAvailable()) {
-                    startActivity(divisionsIntent())
-                    finish()
-                } else {
-                    Toast.makeText(this, "Нет подключения к сети", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                startActivity(loginIntent())
-                finish()
-            }
+        saveState()
+        loadNextActivity()
 //        if (isInternetAvailable()) {
 //
 //
